7aa5739fff3ba468e80de688196d37493201ed9c
+ <jkunins95@gmail.com>
2023-10-14 12:06:42 -0400

    completed lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,15 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {/* Map over the `links` array and create an "a" element for each item */}
+      {links.map((link, index) => (
+        // set `href` attribute dynamically based on the value of `link`
+        <a key={index} href={`#${link}`}>{link}</a>
+      ))}
+    </nav>
+  )
 }
 
 export default NavBar;
